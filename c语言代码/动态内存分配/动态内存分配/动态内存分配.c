826e537485eaffb69d2b63c25dd04a4a3dfd055e
2353984121lizhen <2353984121@QQ.com>
2023-10-12 23:09:06 +0800

    完善

---
+++ c语言代码/动态内存分配/动态内存分配/动态内存分配.c
@@ -0,0 +1,68 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+//struct S {
+//	char name;
+//	int age;
+//};
+//
+//
+//
+//int main() {
+//	int n = 0;
+//	scanf("%d", &n);
+//	//struct S s1[n];   不能放n,因为数组中的个数必须是一个常量
+//	struct S s1[50]; //50个struct S 类型的数据
+//	return 0;
+//}
+
+
+//c语言中,可以创建变长数组,就是自己输入数字,然后设定数组的长度  不过这是在c99中才有,不是所有编译器都支持
+
+
+//动态内存:就是你想使用多大内存你就能使用多大内存
+
+//在堆区使用动态内存分配
+//malloc    free    calloc    realloc   
+
+//先了解malloc     void* malloc(size_t n)  此函数就是向堆区申请的地址,后面的n为申请的字节个数
+//返回类型为地址类型,返回的市首地址,括号里面填的是字节个数,就是申请空间大小
+
+//要引用头文件
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+int main() {
+	//向内存申请10个整型的空间
+	int *p = (int*)malloc(10 * sizeof(int));
+	//如果求取内存过大,超过了计算机所能承受的范围,则返回一个空指针
+	if (p == NULL) {
+		//打印错误原因的方式
+		//使用这个函数需要引用errno.h  头文件
+		printf("%s\n", strerror(errno));  //strerror(errno)  这个函数hi返回错误的原因
+	}
+	else {
+		printf("内存空间开辟成功\n");
+		int i = 0;
+		for (i = 0; i < 10; i++) {
+			*(p + i) = i;
+		}
+		for (i = 0; i < 10; i++) {
+			printf("%d\n",*(p + i));
+		}
+
+
+	}
+
+	//当动态申请的空间不再使用的时候,要将空间释放
+	//free  释放空间
+	free(p);
+	p = NULL;
+	return 0;
+}
+
+
+
+
+
+
