675945f400118eef30f0010c2e6b29c84fe84fd4
2353984121lizhen <2353984121@QQ.com>
2023-10-13 09:36:58 +0800

    完善

--- c语言代码/动态内存分配/动态内存分配/动态内存分配.c
+++ c语言代码/动态内存分配/动态内存分配/动态内存分配.c
@@ -1,6 +1,8 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
 //struct S {
 //	char name;
 //	int age;
@@ -29,40 +31,125 @@
 //返回类型为地址类型,返回的市首地址,括号里面填的是字节个数,就是申请空间大小
 
 //要引用头文件
-#include <stdlib.h>
-#include <string.h>
-#include <errno.h>
-int main() {
-	//向内存申请10个整型的空间
-	int *p = (int*)malloc(10 * sizeof(int));
-	//如果求取内存过大,超过了计算机所能承受的范围,则返回一个空指针
-	if (p == NULL) {
-		//打印错误原因的方式
-		//使用这个函数需要引用errno.h  头文件
-		printf("%s\n", strerror(errno));  //strerror(errno)  这个函数hi返回错误的原因
-	}
-	else {
-		printf("内存空间开辟成功\n");
-		int i = 0;
-		for (i = 0; i < 10; i++) {
-			*(p + i) = i;
-		}
-		for (i = 0; i < 10; i++) {
-			printf("%d\n",*(p + i));
-		}
+
+//int main() {
+//	//向内存申请10个整型的空间
+//	int *p = (int*)malloc(10 * sizeof(int));
+//	//如果求取内存过大,超过了计算机所能承受的范围,则返回一个空指针
+//	if (p == NULL) {
+//		//打印错误原因的方式
+//		//使用这个函数需要引用errno.h  头文件
+//		printf("%s\n", strerror(errno));  //strerror(errno)  这个函数hi返回错误的原因
+//	}
+//	else {
+//		printf("内存空间开辟成功\n");
+//		int i = 0;
+//		for (i = 0; i < 10; i++) {
+//			*(p + i) = i;
+//		}
+//		for (i = 0; i < 10; i++) {
+//			printf("%d\n",*(p + i));
+//		}
+//
+//
+//	}
+//
+//	//当动态申请的空间不再使用的时候,要将空间释放
+//	//free  释放空间
+//	free(p);
+//	p = NULL;
+//	return 0;
+//}
+//
+
+//  colloc  函数
+//int main() {
+//	//使用calloc开辟空间
+//	//这个函数的用法,前面一个数字填元素的个数,后面一个填元素所占的字节个数
+//	int* p= (int*)calloc(10,sizeof(int));
+//	if (p == NULL) {
+//		printf("%s\n", strerror(errno));
+//	}
+//	else {
+//		int i = 0;
+//		for (i = 0; i < 10; i++) {
+//			printf("%d ", *(p + i));
+//		}
+//	}
+//	//释放空间
+//	//free函数用来释放开辟的动态空间
+//	free(p);
+//	p = NULL;
+//	/*主要是 malloc 和 calloc 的区别。 calloc 会申请内存，并
+//	全初始化为0；而 malloc 只申请内存，并不作初始化。 所以 calloc 的执行会比 malloc 稍微费时，因为它多了初始化的步骤*/
+//	return 0;
+//}
 
 
-	}
 
-	//当动态申请的空间不再使用的时候,要将空间释放
-	//free  释放空间
-	free(p);
-	p = NULL;
-	return 0;
-}
+//realloc
+//调整动态开辟内存空间大小
+//int main() {
+//	int *p = (int*)malloc(20);
+//	if (p == NULL) {
+//		printf("%s\n", strerror(errno));
+//	}
+//	else {
+//		int i = 0;
+//		for (i = 0; i < 10; i++) {
+//			*(p + i) = i;
+//		}
+//	}
+//	//使用malloc开辟的20个字节
+//	//假设20个字节不够使用了,需要40个字节
+//	//使用realloc
+//	int* p2 = (int*)realloc(p, 40);
+//
+//	//realloc 使用注意事项
+//	//p = (int*)realloc(p, 40);
+//	//1.如果p指向的空间有足够的空间可以追加,则直接追加,返回p
+//	//2.如果p之后没有足够的空间追加,则realloc重新找一块满足需求的空间开辟,并把原来的数据拷贝过来
+//	//释放掉旧的空间,最后返回新开辟的空间地址
+//	//3.得用一个新的变量来接受realloc函数的返回值
+//	//4.使用开辟内存函数之后,一定要进行判断
+//	if (p2 != NULL) {
+//		p = p2;
+//	}
+//	//释放内存
+//	free(p);
+//	p = NULL;
+//	p2 = NULL;
+//
+//}
 
 
 
+//对动态空间的内存越界访问
+//
+//int main() {
+//	int* p = (int*)malloc(5 * sizeof(int));
+//	if (p == NULL) {
+//		return 0;
+//	}
+//	else {
+//		int i = 0; 
+//		//for (i = 0; i < 10; i++) {  //越界访问
+//		//	*(p + i) = i;
+//		//}  //就会出现错误
+//	}
+//	free(p);
+//	p = NULL;
+//	return 0;
+//}
 
 
-
+//对非动态内存进行释放也会报错
+// 程序也会崩掉
+//int main() {
+//	int a = 10;
+//	int* p = &a;
+//	*p = 20;
+//	free(p);
+//	p = NULL;
+//	return 0;
+//}
